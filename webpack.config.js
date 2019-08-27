98b3005aa39b231e353ed905b7b44d2449370c56
Tarepan <tarepan5884@gmail.com>
2019-08-27 11:34:31 +0900

    Add package publish settings

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.js"
+    filename: "[name].js"
   },
   plugins: [
     new HtmlWebpackPlugin({
